f8755b2c88fac7efcff53e5b0279f9fbd62ea241
Senim <senimelhouar@gmail.com>
2017-07-13 11:59:35 +0200

    Change Url

--- sal7af.sh
+++ sal7af.sh
@@ -4,5 +4,5 @@ WEBSITE=test.com
 for i in {1..99}
 do
     echo Downloading Page $i :
-    curl "https://${WEBSITE}/dubai/search/Pharmacies/page/${i}/tab/firms" > raw/$i.html
+    curl "${WEBSITE}/$i" > raw/$i.html
 done
